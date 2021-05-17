5854a532b7dc8fd73f6a1ddf566bec292359f933
ottoniel.molina <ottonielmolina@gmail.com>
2021-05-16 23:57:59 -0600

    Configuración para despliegue

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/web-concesionario'));
+app.use(express.static(__dirname + '/dist/webConcesionario'));
 
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/web-concesionario/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/webConcesionario/index.html'));
 })
 
 app.listen(process.env.PORT || 8080);
